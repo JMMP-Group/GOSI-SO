c30696a00d1ca9d16d682a81db819f891b2163c3
oceandie <diego.bruciaferri@metoffice.gov.uk>
2025-08-12 11:18:18 +0100

    correcting interpolation U/V->T and hpge only in loc areas

--- src/envelopes/create_2D_hpge_field.py
+++ src/envelopes/create_2D_hpge_field.py
@@ -19,14 +19,20 @@ from matplotlib import pyplot as plt
 import xarray as xr
 #import xarray.ufuncs as xu
 from dask.diagnostics import ProgressBar
+from lib import compute_masks
 
 # ==============================================================================
 # Input files
 # ==============================================================================
 
+# File describing the geometry of the model domain (i.e., domain_cfg.nc)
+DOMfile = '/data/users/catherine.guiavarch/GOSI10/SouthernOcean/GOSI-SO/GOSI-SO_inputs/domain_cfg_antarctica.dep3550_sig3_stn9_itr3_r12_r12.nc'
+
+# File including the localisation mask
+LOCfile = '/data/users/catherine.guiavarch/GOSI10/SouthernOcean/GOSI-SO/GOSI-SO_inputs/bathymetry.loc_area-antarctica.dep3550_sig3_stn9_itr3.MEs_4env_3550_r12_r12.nc'
+
 # Folder path containing HPGE spurious currents velocity files 
-#HPGEdir = '/scratch/dbruciaf/GOSI10p1.0-hpge/r12_r16-r075-r040-r035_it2-r030'
-HPGEdir = '/data/users/catherine.guiavarch/GOSI10/SouthernOcean/HPG_SO/u-dr701/run4_prof11/'
+HPGEdir = '/data/scratch/diego.bruciaferri/HPG_SO/u-dr701/run3_prof10/'
 
 # List of indexes of the last T-level of each vertical subdomains 
 # (Fortran indexening convention)
@@ -41,6 +47,20 @@ chunk_var = 'time_counter'
 chunk_size = 1
 
 # ==============================================================================
+
+# Computing volume of model cells
+ds_d = xr.open_dataset(DOMfile, chunks={}, drop_variables=("nav_lev","time_counter"))
+ds_d = ds_d.rename_dims({'nav_lev':'z','time_counter':'t'}).squeeze()
+ds_d = compute_masks(ds_d, merge=True)
+volT = ds_d.e1t * ds_d.e2t * ds_d.e3t_0
+volU = ds_d.e1u * ds_d.e2u * ds_d.e3u_0
+volV = ds_d.e1v * ds_d.e2v * ds_d.e3v_0
+
+# Localisation mask
+ds_l = xr.open_dataset(LOCfile)
+Lmsk = ds_l.s2z_msk
+Lmsk = Lmsk.where(Lmsk==0,1)
+
 # LOOP
 
 Ufiles = sorted(glob.glob(HPGEdir+'/*grid_U*.nc'))
@@ -50,23 +70,31 @@ for F in range(len(Ufiles)):
 
     print(Ufiles[F])
 
-    ds_U = xr.open_dataset(Ufiles[F], chunks={chunk_var:chunk_size})
-    U4   = ds_U[Uvar]
-    ds_V = xr.open_dataset(Vfiles[F], chunks={chunk_var:chunk_size})
-    V4   = ds_V[Vvar]
+    ds_U = xr.open_dataset(Ufiles[F], chunks={chunk_var:chunk_size}, 
+                                      drop_variables=("depthu","time_counter")
+           )
+    u4   = ds_U[Uvar]
+    ds_V = xr.open_dataset(Vfiles[F], chunks={chunk_var:chunk_size}, 
+                                      drop_variables=("depthv","time_counter")
+           )
+    v4   = ds_V[Vvar]
 
     # rename some dimensions
-    U4 = U4.rename({U4.dims[0]: 't', U4.dims[1]: 'k'})
-    V4 = V4.rename({V4.dims[0]: 't', V4.dims[1]: 'k'})
+    u4 = u4.rename({"time_counter": 't', "depthu": 'z'})
+    v4 = v4.rename({"time_counter": 't', "depthv": 'z'})
+
+    #print(u4)
+    #print(u4.t)
+    #print(v4.shape)
 
     #print("U4 max:", np.nanmax(U4))
     #print("V4 max:", np.nanmax(V4))
 
     # interpolating from U,V to T
-    U = U4.rolling({'x':2}).mean().fillna(0.)
-    V = V4.rolling({'y':2}).mean().fillna(0.) 
+    UT = (u4 * volU).rolling({'x':2}).mean().fillna(0.) / volT
+    VT = (v4 * volV).rolling({'y':2}).mean().fillna(0.) / volT 
     
-    hpge = np.sqrt(np.power(U,2) + np.power(V,2))
+    hpge = np.sqrt(np.power(UT,2) + np.power(VT,2)) * ds_d.tmask
 
     #print("hpge max:", np.nanmax(hpge))
 
@@ -82,27 +110,27 @@ for F in range(len(Ufiles)):
           max_hpge1 = np.zeros(shape=(nj,ni))
 
     if len(num_lev) > 1:
-       maxhpge_1 = hpge.isel(k=slice(None, num_lev[0])).max(dim='k').max(dim='t')
-       maxhpge_2 = hpge.isel(k=slice(num_lev[0], num_lev[1])).max(dim='k').max(dim='t')
-       maxhpge_3 = hpge.isel(k=slice(num_lev[1], num_lev[2])).max(dim='k').max(dim='t')
-       maxhpge_4 = hpge.isel(k=slice(num_lev[2], num_lev[3])).max(dim='k').max(dim='t')
+       maxhpge_1 = hpge.isel(z=slice(None, num_lev[0])).max(dim='z').max(dim='t')
+       maxhpge_2 = hpge.isel(z=slice(num_lev[0], num_lev[1])).max(dim='z').max(dim='t')
+       maxhpge_3 = hpge.isel(z=slice(num_lev[1], num_lev[2])).max(dim='z').max(dim='t')
+       maxhpge_4 = hpge.isel(z=slice(num_lev[2], num_lev[3])).max(dim='z').max(dim='t')
        max_hpge1 = np.maximum(max_hpge1, maxhpge_1.data)
        max_hpge2 = np.maximum(max_hpge2, maxhpge_2.data)
        max_hpge3 = np.maximum(max_hpge3, maxhpge_3.data)
        max_hpge4 = np.maximum(max_hpge4, maxhpge_4.data)
     else:
-       maxhpge_1 = hpge.isel(k=slice(None, num_lev[0])).max(dim='k').max(dim='t')
+       maxhpge_1 = hpge.isel(z=slice(None, num_lev[0])).max(dim='z').max(dim='t')
        max_hpge1 = np.maximum(max_hpge1, maxhpge_1.data)
 
 # Saving 
 ds_hpge = xr.Dataset()
 if len(num_lev) > 1:
-   ds_hpge["max_hpge_1"] = xr.DataArray(max_hpge1, dims=('y','x'))
-   ds_hpge["max_hpge_2"] = xr.DataArray(max_hpge2, dims=('y','x'))
-   ds_hpge["max_hpge_3"] = xr.DataArray(max_hpge3, dims=('y','x'))
-   ds_hpge["max_hpge_4"] = xr.DataArray(max_hpge4, dims=('y','x'))
+   ds_hpge["max_hpge_1"] = xr.DataArray(max_hpge1*Lmsk, dims=('y','x'))
+   ds_hpge["max_hpge_2"] = xr.DataArray(max_hpge2*Lmsk, dims=('y','x'))
+   ds_hpge["max_hpge_3"] = xr.DataArray(max_hpge3*Lmsk, dims=('y','x'))
+   ds_hpge["max_hpge_4"] = xr.DataArray(max_hpge4*Lmsk, dims=('y','x'))
 else:
-   ds_hpge["max_hpge_1"] = xr.DataArray(max_hpge1, dims=('y','x'))
+   ds_hpge["max_hpge_1"] = xr.DataArray(max_hpge1*Lmsk, dims=('y','x'))
 
 # -------------------------------------------------------------------------------------   
 # Writing the max_hpge file
--- src/envelopes/lib.py
+++ src/envelopes/lib.py
@@ -48,6 +48,58 @@ def read_envInfo(filepath):
     return envInfo
 
 #=======================================================================================
+def compute_masks(ds_domain, merge=False):
+    """
+    Compute masks from domain_cfg Dataset.
+    If merge=True, merge with the input dataset.
+    Parameters
+    ----------
+    ds_domain: xr.Dataset
+        domain_cfg datatset
+    add: bool
+        if True, merge with ds_domain
+    Returns
+    -------
+    ds_mask: xr.Dataset
+        dataset with masks
+    """
+
+    # Extract variables
+    k = ds_domain["z"] + 1
+    top_level = ds_domain["top_level"]
+    bottom_level = ds_domain["bottom_level"]
+
+    # Page 27 NEMO book.
+    # I think there's a typo though.
+    # It should be:
+    #                  | 0 if k < top_level(i, j)
+    # tmask(i, j, k) = | 1 if top_level(i, j) ≤ k ≤ bottom_level(i, j)
+    #                  | 0 if k > bottom_level(i, j)
+    tmask = xr.where(np.logical_or(k < top_level, k > bottom_level), 0, np.nan)
+    tmask = xr.where(np.logical_and(bottom_level >= k, top_level <= k), 1, tmask)
+    tmask = tmask.rename("tmask")
+
+    tmask = tmask.transpose("z","y","x")
+
+    # Need to shift and replace last row/colum with tmask
+    # umask(i, j, k) = tmask(i, j, k) ∗ tmask(i + 1, j, k)
+    umask = tmask.rolling(x=2).prod().shift(x=-1)
+    umask = umask.where(umask.notnull(), tmask)
+    umask = umask.rename("umask")
+
+    # vmask(i, j, k) = tmask(i, j, k) ∗ tmask(i, j + 1, k)
+    vmask = tmask.rolling(y=2).prod().shift(y=-1)
+    vmask = vmask.where(vmask.notnull(), tmask)
+    vmask = vmask.rename("vmask")
+
+    # Return
+    masks = xr.merge([tmask, umask, vmask])
+    if merge:
+        return xr.merge([ds_domain, masks])
+    else:
+        return masks
+
+#=======================================================================================
 def calc_zenv(bathy, surf, offset, max_dep, loc_msk):
     """
     Constructs an envelop bathymetry 
